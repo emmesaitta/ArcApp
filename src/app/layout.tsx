714181d0fd7a2a8c1c5abe0089fce0c7f05d5503
emmesaitta <emmesaitta@gmail.com>
2026-04-02 21:06:48 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import {
-  LayoutDashboard, Users, FileText, Utensils,
+  Users, FileText, Utensils,
   CalendarDays, UserCheck, Menu, X
 } from 'lucide-react'
 import { useState } from 'react'
